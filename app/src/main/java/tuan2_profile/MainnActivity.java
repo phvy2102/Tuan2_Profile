7d29ef239a64f266c94f10e044530026f38a26a4
phvy2102 <phvy21022002@gmail.com>
2022-09-12 23:46:37 +0700

    tuan2

---
+++ app/src/main/java/tuan2_profile/MainnActivity.java
@@ -0,0 +1,13 @@
+package tuan2_profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainn);
+    }
+}
